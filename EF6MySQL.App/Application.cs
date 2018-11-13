7ca0d87359e289c134f01fb3c11225aadf743788
HENGLEASING\pichid.de <pichid.de@hengleasing.co.th>
2018-11-13 22:27:47 +0700

    Add files

---
+++ EF6MySQL.App/Application.cs
@@ -0,0 +1,22 @@
+﻿using EF6MySQL.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EF6MySQL.App
+{
+    public class Application
+    {
+        public void Run()
+        {
+            using (MasterDataEntities db = new MasterDataEntities())
+            {
+                List<province> provinces = db.provinces
+                    .Where(it => it.REGION_ID == "02")
+                    .ToList();
+
+                Console.WriteLine("Total: {0}", provinces.Count);
+            }
+        }
+    }
+}
--- EF6MySQL.App/Program.cs
+++ EF6MySQL.App/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EF6MySQL.App
 {
@@ -10,6 +6,10 @@ namespace EF6MySQL.App
     {
         static void Main(string[] args)
         {
+            Application application = new Application();
+            application.Run();
+
+            Console.ReadKey();
         }
     }
 }
